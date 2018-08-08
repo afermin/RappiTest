e2aee33600e7e80ec351e633c62c7c8d27ed6a18
Alexander Fermín <alexfer06@gmail.com>
2018-08-08 15:45:54 -0500

    - Pagination classes were improved

---
+++ app/src/main/java/com/example/rappitest/repository/FetchNextSearchPageTask.kt
@@ -0,0 +1,80 @@
+package com.example.rappitest.repository
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import com.example.rappitest.api.*
+import com.example.rappitest.db.SearchDB
+import com.example.rappitest.vo.ProductionBaseData
+import com.example.rappitest.vo.Resource
+import com.example.rappitest.vo.SearchResult
+import retrofit2.Response
+import java.io.IOException
+
+/**
+ * Created by Alexander Fermin (alexfer06@gmail.com) on 08/08/2018.
+ */
+abstract class FetchNextSearchPageTask<T : ProductionBaseData> constructor(
+        private val category: String,
+        private val db: SearchDB
+) : Runnable {
+
+    private val _liveData = MutableLiveData<Resource<Boolean>>()
+    val liveData: LiveData<Resource<Boolean>> = _liveData
+    protected var nextPage = 0
+
+    override fun run() {
+        val current = db.searchResultDao().findSearchResult(category)
+        if (current == null) {
+            _liveData.postValue(null)
+            return
+        }
+        val page = current.page
+        if (page == current.totalCount) {
+            _liveData.postValue(Resource.success(false))
+            return
+        }
+        val newValue = try {
+            nextPage = page + 1
+            val response = getRequest()
+
+            val apiResponse = ApiResponse.create(response)
+            when (apiResponse) {
+                is ApiSuccessResponse -> {
+                    // we merge all ids into 1 list so that it is easier to fetch the
+                    // result list.
+                    val ids = arrayListOf<Int>()
+                    ids.addAll(current.repoIds)
+
+                    ids.addAll(apiResponse.body.results.map { it.id })
+                    val merged = SearchResult(
+                            category, ids,
+                            apiResponse.body.totalPages, apiResponse.body.page
+                    )
+                    try {
+                        db.beginTransaction()
+                        db.searchResultDao().insert(merged)
+                        insertResult(apiResponse)
+                        db.setTransactionSuccessful()
+                    } finally {
+                        db.endTransaction()
+                    }
+                    Resource.success(apiResponse.body.page != apiResponse.body.totalPages)
+                }
+                is ApiEmptyResponse -> {
+                    Resource.success(false)
+                }
+                is ApiErrorResponse -> {
+                    Resource.error(apiResponse.errorMessage, true)
+                }
+            }
+
+        } catch (e: IOException) {
+            Resource.error(e.message!!, true)
+        }
+        _liveData.postValue(newValue)
+    }
+
+    abstract fun insertResult(apiResponse: ApiSuccessResponse<SearchResponse<T>>)
+
+    abstract fun getRequest(): Response<SearchResponse<T>>
+}
--- app/src/main/java/com/example/rappitest/repository/MovieFetchNextSearchPageTask.kt
+++ app/src/main/java/com/example/rappitest/repository/MovieFetchNextSearchPageTask.kt
@@ -1,16 +1,11 @@
 package com.example.rappitest.repository
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
-import com.example.rappitest.api.ApiEmptyResponse
-import com.example.rappitest.api.ApiErrorResponse
-import com.example.rappitest.api.ApiResponse
 import com.example.rappitest.api.ApiSuccessResponse
-import com.example.rappitest.vo.Resource
 import com.example.rappitest.api.MovieService
+import com.example.rappitest.api.SearchResponse
 import com.example.rappitest.db.SearchDB
-import com.example.rappitest.vo.SearchResult
-import java.io.IOException
+import com.example.rappitest.vo.movie.Movie
+import retrofit2.Response
 
 /**
  * A task that reads the search result in the database and fetches the page page, if it has one.
@@ -19,25 +14,15 @@ class MovieFetchNextSearchPageTask constructor(
         private val movieService: MovieService,
         private val category: String,
         private val db: SearchDB
-) : Runnable {
-    private val _liveData = MutableLiveData<Resource<Boolean>>()
-    val liveData: LiveData<Resource<Boolean>> = _liveData
+) : FetchNextSearchPageTask<Movie>(category, db) {
 
-    override fun run() {
-        val current = db.searchResultDao().findSearchResult(category)
-        if (current == null) {
-            _liveData.postValue(null)
-            return
-        }
-        val page = current.page
-        if (page == current.totalCount) {
-            _liveData.postValue(Resource.success(false))
-            return
-        }
-        val newValue = try {
 
-            val nextPage = page + 1
-            val response = when (category) {
+    override fun insertResult(apiResponse: ApiSuccessResponse<SearchResponse<Movie>>) {
+        db.movieDao().insertMovie(apiResponse.body.results)
+    }
+
+    override fun getRequest(): Response<SearchResponse<Movie>> =
+            when (category) {
                 MovieRepository.Category.MOVIE_TOP_RATED.name ->
                     movieService.getTopRated(nextPage).execute()
                 MovieRepository.Category.MOVIE_UPCOMING.name ->
@@ -46,40 +31,4 @@ class MovieFetchNextSearchPageTask constructor(
                     movieService.getPopular(nextPage).execute()
             }
 
-            val apiResponse = ApiResponse.create(response)
-            when (apiResponse) {
-                is ApiSuccessResponse -> {
-                    // we merge all ids into 1 list so that it is easier to fetch the
-                    // result list.
-                    val ids = arrayListOf<Int>()
-                    ids.addAll(current.repoIds)
-
-                    ids.addAll(apiResponse.body.results.map { it.id })
-                    val merged = SearchResult(
-                            category, ids,
-                            apiResponse.body.totalPages, apiResponse.body.page
-                    )
-                    try {
-                        db.beginTransaction()
-                        db.searchResultDao().insert(merged)
-                        db.movieDao().insertMovie(apiResponse.body.results)
-                        db.setTransactionSuccessful()
-                    } finally {
-                        db.endTransaction()
-                    }
-                    Resource.success(apiResponse.body.page != apiResponse.body.totalPages)
-                }
-                is ApiEmptyResponse -> {
-                    Resource.success(false)
-                }
-                is ApiErrorResponse -> {
-                    Resource.error(apiResponse.errorMessage, true)
-                }
-            }
-
-        } catch (e: IOException) {
-            Resource.error(e.message!!, true)
-        }
-        _liveData.postValue(newValue)
-    }
 }
